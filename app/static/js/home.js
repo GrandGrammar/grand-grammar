e4539d38b7e6f8f019fcb79a73f2b77b4cd4cb27
lizhuo <lizhuogo@gmail.com>
2017-11-26 10:06:32 -0600

    typed slogon

---
+++ app/static/js/home.js
@@ -0,0 +1,10 @@
+var options = {
+  strings: ["Improv^1000ing your essay today!", "Improv^1000e your essay today!"],
+  typeSpeed: 60,
+  backSpeed: 40,
+  smartBackspace: true,
+  backDelay: 700,
+  showCursor: false
+}
+
+var typed = new Typed("#slogon", options);
